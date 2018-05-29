5697750737d4e94ba4641737fe0b4ac410b6d36a
loki <hjqi@quant-chi.com>
2018-05-29 16:07:37 +0800

    修复spring配置问题

--- src/main/java/com/quantchi/transport/service/SearchApiService.java
+++ src/main/java/com/quantchi/transport/service/SearchApiService.java
@@ -10,8 +10,8 @@ import org.springframework.stereotype.Service;
 public class SearchApiService {
 	private static final Logger logger = LoggerFactory.getLogger(SearchApiService.class);
 
-	@Autowired
-	private HttpSolrClient server;
+	/*@Autowired
+	private HttpSolrClient server;*/
 
 	public void search(){
 		/*SolrQuery query = new SolrQuery();
