1d04063fb9ccf78584891a6291b7858a9e77826d
loki <hjqi@quant-chi.com>
2018-05-30 16:27:55 +0800

    1.即时搜索

--- src/main/java/com/quantchi/transport/controller/SearchApiController.java
+++ src/main/java/com/quantchi/transport/controller/SearchApiController.java
@@ -3,6 +3,7 @@ package com.quantchi.transport.controller;
 
 import com.quantchi.common.util;
 import com.quantchi.transport.service.SearchApiService;
+import org.apache.solr.client.solrj.response.QueryResponse;
 import org.apache.solr.common.SolrDocumentList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -25,20 +26,20 @@ public class SearchApiController {
         searchApiService.isIndex(q);
 
 
-
-        SolrDocumentList rets = searchApiService.search(q);
+        QueryResponse rets = searchApiService.search(q);
 
         //处理后的结果集
-        SolrDocumentList afterHandle = searchApiService.handle(q,rets);
+        SolrDocumentList afterHandle = searchApiService.handle(q,rets.getResults());
 
-        return util.genRet(200,rets,"ok",rets.size());
+        return util.genRet(200,rets,"ok",rets.getResults().size());
     }
 
     @RequestMapping(value = "/queryInstance", method = { RequestMethod.GET })
     public @ResponseBody
     Map<String, Object> queryInstance (@RequestParam("q") String q) throws Exception {
-        SolrDocumentList rets = searchApiService.searchInstance(q);
-        return util.genRet(200,rets,"ok",rets.size());
+        QueryResponse rets = searchApiService.searchInstance(q);
+        SolrDocumentList data =  searchApiService.handleInst(q,rets);
+        return util.genRet(200,rets,"ok",rets.getResults().size());
     }
 
 
