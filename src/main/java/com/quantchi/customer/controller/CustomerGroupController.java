ff2e762dc8a4f8495786b6f60bddba120f7e3241
jxg <495373469@qq.com>
2018-06-13 11:23:45 +0800

    客群相关接口

--- src/main/java/com/quantchi/customer/controller/CustomerGroupController.java
+++ src/main/java/com/quantchi/customer/controller/CustomerGroupController.java
@@ -2,7 +2,9 @@ package com.quantchi.customer.controller;
 
 import com.quantchi.customer.pojo.CustomerGroup;
 import com.quantchi.customer.service.CustomerGroupService;
+import java.util.List;
 import java.util.Map;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,9 +25,61 @@ public class CustomerGroupController {
   //客群列表查看
   @ResponseBody
   @RequestMapping(value = "/listCustomerGroups", method = {RequestMethod.POST})
-  public Map<String, Object> listCustomerGroups(@RequestBody CustomerGroup role, Integer pageIndex,
-      Integer pagesize) {
-    return service.selectCustomerGroup(role, pageIndex, pagesize);
+  public Map<String, Object> listCustomerGroups(@RequestBody CustomerGroup group) {
+    Integer pageIndex = group.getPageIndex();
+    Integer pagesize = group.getPagesize();
+    return service.selectCustomerGroup(group, pageIndex, pagesize);
+  }
+
+  //客群列表添加
+  @ResponseBody
+  @RequestMapping(value = "/createCustomerGroup", method = {RequestMethod.POST})
+  public Map<String, Object> createCustomerGroup(@RequestBody Map<String, Object> map) {
+    CustomerGroup group = new CustomerGroup();
+    group.setCust_group_name(map.get("cust_group_name").toString());
+    group.setCreate_user_id(map.get("create_user_id").toString());
+    group.setCust_nums(Integer.valueOf(map.get("cust_nums").toString()));
+    List<Map<String, String>> customerGroupCriteria = (List<Map<String, String>>) map
+        .get("CustomerGroupCriteria");
+    group.setCondition_nums(customerGroupCriteria.size());
+    return service.createCustomerGroup(group, customerGroupCriteria);
+  }
+
+
+  //客群列表删除
+  @ResponseBody
+  @RequestMapping(value = "/deleteCustomerGroup", method = {RequestMethod.POST})
+  public Map<String, Object> deleteCustomerGroup(@RequestBody CustomerGroup group) {
+    return service.deleteCustomerGroup(group);
+  }
+
+  //客群列表更新
+  @ResponseBody
+  @RequestMapping(value = "/updateCustomerGroup", method = {RequestMethod.POST})
+  public Map<String, Object> updateCustomerGroup(@RequestBody CustomerGroup group) {
+    return service.updateCustomerGroup(group);
+  }
+
+  //客群客户列表结果导出
+  @ResponseBody
+  @RequestMapping(value = "/exportCustomerList", method = {RequestMethod.POST})
+  public Map<String, Object> exportCustomerList(@RequestBody CustomerGroup group,
+      HttpServletResponse response) {
+
+    String fileName = "客群分析-个体详情";
+    String title = "客群分析-个体详情";
+    String[] titles = {"客户号", "客户姓名", "融资负债（万元）", "总资产（万元）", "维保比例", "当前仓位", "年度收益率", "选股成功率",
+        "买卖正确率", "平均仓位", "平均", "亏损率"};
+    //String msg = ExportUtil.exportRelationExcel(response, fileName, title, titles, mappingData);
+    return service.updateCustomerGroup(group);
+  }
+
+  //客群客户详情列表展示
+  @ResponseBody
+  @RequestMapping(value = "/listCustomersByCustomerGroupId", method = {RequestMethod.POST})
+  public Map<String, Object> listCustomersByCustomerGroupId(@RequestBody CustomerGroup group) {
+
+    return service.updateCustomerGroup(group);
   }
 
 
--- src/main/java/com/quantchi/customer/serviceImpl/CustomerGroupServiceImpl.java
+++ src/main/java/com/quantchi/customer/serviceImpl/CustomerGroupServiceImpl.java
@@ -2,6 +2,7 @@ package com.quantchi.customer.serviceImpl;
 
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
+import com.quantchi.common.util;
 import com.quantchi.customer.mapper.CustomerGroupMapper;
 import com.quantchi.customer.pojo.CustomerGroup;
 import com.quantchi.customer.service.CustomerGroupService;
@@ -23,18 +24,84 @@ public class CustomerGroupServiceImpl implements CustomerGroupService {
   @Override
   public Map<String, Object> selectCustomerGroup(CustomerGroup customerGroup, Integer pageIndex,
       Integer pagesize) {
-
-    PageHelper.startPage(pageIndex, pagesize);
-    // 执行查询
-    List<CustomerGroup> list = mapper.selectCustomerGroup(customerGroup);
-    // 取分页信息
-    PageInfo<CustomerGroup> pageInfo = new PageInfo<>(list);
-    // 返回处理结果
-    Map<String,Object> result = new HashMap<String,Object>();
-    // 获取总条数
-    result.put("Total",pageInfo.getTotal());
-    // 结果rows数据
-    result.put("Rows",list);
-    return result;
+    try {
+      PageHelper.startPage(pageIndex, pagesize);
+      // 执行查询
+      List<CustomerGroup> list = mapper.selectCustomerGroup(customerGroup);
+      // 取分页信息
+      PageInfo<CustomerGroup> pageInfo = new PageInfo<>(list);
+      // 返回处理结果
+      Map<String, Object> result = new HashMap<String, Object>();
+      // 获取总条数
+      result.put("total", pageInfo.getTotal());
+      // 结果rows数据
+      result.put("data", pageInfo.getList());
+      return util
+          .genRet(200, result.get("data"), "ok", Integer.parseInt(result.get("total").toString()));
+    } catch (Exception e) {
+      e.printStackTrace();
+      return util.genRet(500, null, "select CustomerGroup error", 0);
+    }
   }
+
+  public Map<String, Object> createCustomerGroup(CustomerGroup group,List<Map<String,String>> customerGroupCriteria) {
+    Map<String, Object> result = new HashMap<String, Object>();
+    try {
+      String str = mapper.selectCustGroupId();
+      if (str==null){
+        group.setCust_group_id("CG000001");
+      }
+      if(customerGroupCriteria.isEmpty()||customerGroupCriteria==null){
+        result.put("code","500");
+        result.put("msg","条件不能为空！");
+        return result;
+      }
+
+      group.setCondition_desc(customerGroupCriteria.get(0).get("value"));
+      group.setCust_group_id(str);
+      group.setRefresh_status("0");
+      group.setDelete_status("0");
+      group.setHistory_status("0");
+      mapper.createCustomerGroup(group);
+      result.put("code","200");
+      result.put("msg","ok");
+      return result;
+    } catch (Exception e) {
+      e.printStackTrace();
+      result.put("code","500");
+      result.put("msg","error");
+      return result;
+    }
+  }
+
+  public Map<String, Object> deleteCustomerGroup(CustomerGroup group) {
+    Map<String, Object> result = new HashMap<String, Object>();
+    try {
+      mapper.deleteCustomerGroup(group);
+      result.put("code","200");
+      result.put("msg","ok");
+      return result;
+    } catch (Exception e) {
+      e.printStackTrace();
+      result.put("code","500");
+      result.put("msg","error");
+      return result;
+    }
+  }
+  public Map<String, Object> updateCustomerGroup(CustomerGroup group){
+    Map<String, Object> result = new HashMap<String, Object>();
+    try {
+      mapper.updateCustomerGroup(group);
+      result.put("code","200");
+      result.put("msg","ok");
+      return result;
+    } catch (Exception e) {
+      e.printStackTrace();
+      result.put("code","500");
+      result.put("msg","error");
+      return result;
+    }
+  }
+
+  //selectTerm
 }
