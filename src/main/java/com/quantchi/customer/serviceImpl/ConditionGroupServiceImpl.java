23e209a7e8f147fac32a4c8ce60d92b65ae0ef82
byhan <byhan@quant-chi.com>
2018-06-14 11:28:32 +0800

    1.applicationContext.xml中添加mybatis分页配置 2.修复按照id查询空指针异常

--- src/main/java/com/quantchi/customer/serviceImpl/ConditionGroupServiceImpl.java
+++ src/main/java/com/quantchi/customer/serviceImpl/ConditionGroupServiceImpl.java
@@ -65,33 +65,42 @@ public class ConditionGroupServiceImpl implements ConditionGroupService {
         ConditionGroup conditionGroup = null;
         Map<String, Object> map = new HashMap<>();
         List<Map<String, Object>> list = new ArrayList<>();
-        conditionGroup = conditionGroupMapper.findCustomerGroup(customerGroupId);
-        map.put("cust_group_name",conditionGroup.getCust_group_name());
-        map.put("create_user_id",conditionGroup.getCreate_user_id());
-        String condition_desc = conditionGroup.getCondition_desc();
-        String condition_desc_id = conditionGroup.getCondition_desc_id();
-        String[] condition_descs = condition_desc.split("\\|");
-        String[] condition_desc_ids = condition_desc_id.split("\\|");
-        for(int i=0; i<condition_descs.length;i++){
-            Map<String,Object> conditionMap = new HashMap<>();
-            String[] desc_id = condition_desc_ids[i].split(":");
-            conditionMap.put("id",desc_id[1].split(",")[0]);
-            conditionMap.put("type",desc_id[2]);
-            String[] desc = condition_descs[i].split(":");
-            if(!conditionMap.get("type").equals("value")){
+        try{
+            conditionGroup = conditionGroupMapper.findCustomerGroup(customerGroupId);
+            map.put("cust_group_name",conditionGroup.getCust_group_name());
+            map.put("create_user_id",conditionGroup.getCreate_user_id());
+            String condition_desc = conditionGroup.getCondition_desc();
+            String condition_desc_id = conditionGroup.getCondition_desc_id();
+            String[] condition_descs = condition_desc.split("\\|");
+            String[] condition_desc_ids = condition_desc_id.split("\\|");
+            for(int i=0; i<condition_descs.length;i++){
+                Map<String,Object> conditionMap = new HashMap<>();
+                String[] desc_id = condition_desc_ids[i].split(":");
+                conditionMap.put("id",desc_id[1].split(",")[0]);
+                conditionMap.put("type",desc_id[2]);
+                String[] desc = condition_descs[i].split(":");
+                if(!conditionMap.get("type").equals("value")){
 
-                conditionMap.put("name",desc[0]);
-                String[] s = desc[1].split(",");
-                conditionMap.put("value",s);
-            }else{
-                conditionMap.put("name",desc[0]);
-                conditionMap.put("value",desc[1]);
+                    conditionMap.put("name",desc[0]);
+                    String[] s = desc[1].split(",");
+                    conditionMap.put("value",s);
+                }else{
+                    conditionMap.put("name",desc[0]);
+                    conditionMap.put("value",desc[1]);
+                }
+                list.add(conditionMap);
             }
-            list.add(conditionMap);
+
+            map.put("CustomerGroupCriteria",list);
+            return map;
+
+        }catch (Exception e){
+            e.printStackTrace();
+            map.put("code",500);
+            map.put("msg",e.getMessage());
+            return map;
         }
 
-        map.put("CustomerGroupCriteria",list);
-        return map;
     }
 
     @Override
