f87e6a8d1efcb55354961a38c92ab245da28ce0c
byhan <byhan@quant-chi.com>
2018-07-04 18:27:32 +0800

    修改term接口注解api

--- src/main/java/com/quantchi/termInfo/controller/TermInfoController.java
+++ src/main/java/com/quantchi/termInfo/controller/TermInfoController.java
@@ -24,51 +24,53 @@ public class TermInfoController {
   @Autowired
   private TermInfoService service;
 
-  /*
+  /**
   * @api {get} /term 术语查询接口
+  * @apiVersion 1.0.0
+  * @apiSampleRequest http://192.168.2.61:8082/quantchiAPI/term
   * @apiName term
   * @apiGroup TermInfoController
-  * @apiParam {Integer} nums
-  * @apiParam {String} entityDesc
-    * @apiSuccess {String} code 成功或者错误代码200成功，500错误
+  * @apiParam {Integer} [nums]
+  * @apiParam {String} [entityDesc]
+  * @apiSuccess {String} code 成功或者错误代码200成功，500错误
   * @apiSuccess {String} msg  成功或者错误信息
-  * @apiSuccess {List} data 返回数据
-  * @apiSuccess {List} entitys 实体信息
-  * @apiSuccess {String} data.entitys.entityType 实体类型
-  * @apiSuccess {String} data.entitys.entityId 实体id
-  * @apiSuccess {String} data.entitys.entityHash 实体hash码
-  * @apiSuccess {String} data.entitys.entityName 实体名
-  * @apiSuccess {String} data.entitys.entityDesc 实体描述
-  * @apiSuccess {String} data.entitys.entityAlias 实体别名
-  * @apiSuccess {String} data.entitys.businessRule 规范描述
-  * @apiSuccess {String} data.entitys.techniqueRule sql语句
-  * @apiSuccess {String} data.entitys.entityStatus 实体状态
-  * @apiSuccess {String} data.entitys.controlDept 管理部门
-  * @apiSuccess {String} data.entitys.assistDept 分配部门
-  * @apiSuccess {String} data.entitys.devPolicy 个人信息分类
-  * @apiSuccess {String} data.entitys.regulatory
-  * @apiSuccess {String} data.entitys.logicType
-  * @apiSuccess {String} data.entitys.displayType 数据显示类型
-  * @apiSuccess {List} data.entitys.physicalField 物理字段
-  * @apiSuccess {String} data.entitys.physicalField.entityId 实体id
-  * @apiSuccess {String} data.entitys.physicalField.physicalFieldHash 物理字段hash码
-  * @apiSuccess {String} data.entitys.physicalField.physicalFieldId 物理字段id
-  * @apiSuccess {String} data.entitys.physicalField.physicalFieldDesc 物理字段描述
-  * @apiSuccess {String} data.entitys.physicalField.physicalTable 物理表
-  * @apiSuccess {String} data.entitys.physicalField.physicalDB 物理数据库
-  * @apiSuccess {String} data.entitys.physicalField.dataType 数据类型
-  * @apiSuccess {String} data.entitys.physicalField.dataLength 数据长度
-  * @apiSuccess {String} data.entitys.physicalField.dataPattern
-  * @apiSuccess {String} data.entitys.physicalField.dataUnit
-  * @apiSuccess {String} data.entitys.physicalField.fieldPartition
-  * @apiSuccess {List} data.entitys.physicalField.dataUDC 条件数据
-  * @apiSuccess {String} data.entitys.physicalField.dataUDC.dataUDCCode
-  * @apiSuccess {String} data.entitys.physicalField.dataUDC.dataUDCDesc 数据描述
-  * @apiSuccess {String} data.entitys.physicalField.dataUDC.dataUDCRule 数据规范
-  * @apiSuccess {String} data.entitys.physicalField.dataUDC.dataUDCValue 数据值
-  * @apiSuccess {String} data.entitys.physicalField.dataUDC.entityHash 数据hash码
-  * @apiSuccess {String} data.entitys.physicalField.dataUDC.entityId 数据id
-  * @apiSuccess {String} data.entitys.physicalField.dataUDC.entityType 数据类型
+  * @apiSuccess {List} [data] 返回数据
+  * @apiSuccess {List} [entitys] 实体信息
+  * @apiSuccess {String} [data.entitys.entityType] 实体类型
+  * @apiSuccess {String} [data.entitys.entityId] 实体id
+  * @apiSuccess {String} [data.entitys.entityHash] 实体hash码
+  * @apiSuccess {String} [data.entitys.entityName] 实体名
+  * @apiSuccess {String} [data.entitys.entityDesc] 实体描述
+  * @apiSuccess {String} [data.entitys.entityAlias] 实体别名
+  * @apiSuccess {String} [data.entitys.businessRule] 规范描述
+  * @apiSuccess {String} [data.entitys.techniqueRule] sql语句
+  * @apiSuccess {String} [data.entitys.entityStatus] 实体状态
+  * @apiSuccess {String} [data.entitys.controlDept] 管理部门
+  * @apiSuccess {String} [data.entitys.assistDept] 分配部门
+  * @apiSuccess {String} [data.entitys.devPolicy] 个人信息分类
+  * @apiSuccess {String} [data.entitys.regulatory]
+  * @apiSuccess {String} [data.entitys.logicType]
+  * @apiSuccess {String} [data.entitys.displayType] 数据显示类型
+  * @apiSuccess {List} [data.entitys.physicalField] 物理字段
+  * @apiSuccess {String} [data.entitys.physicalField.entityId] 实体id
+  * @apiSuccess {String} [data.entitys.physicalField.physicalFieldHash] 物理字段hash码
+  * @apiSuccess {String} [data.entitys.physicalField.physicalFieldId] 物理字段id
+  * @apiSuccess {String} [data.entitys.physicalField.physicalFieldDesc] 物理字段描述
+  * @apiSuccess {String} [data.entitys.physicalField.physicalTable] 物理表
+  * @apiSuccess {String} [data.entitys.physicalField.physicalDB] 物理数据库
+  * @apiSuccess {String} [data.entitys.physicalField.dataType] 数据类型
+  * @apiSuccess {String} [data.entitys.physicalField.dataLength] 数据长度
+  * @apiSuccess {String} [data.entitys.physicalField.dataPattern]
+  * @apiSuccess {String} [data.entitys.physicalField.dataUnit]
+  * @apiSuccess {String} [data.entitys.physicalField.fieldPartition]
+  * @apiSuccess {List} [data.entitys.physicalField.dataUDC] 条件数据
+  * @apiSuccess {String} [data.entitys.physicalField.dataUDC.dataUDCCode]
+  * @apiSuccess {String} [data.entitys.physicalField.dataUDC.dataUDCDesc] 数据描述
+  * @apiSuccess {String} [data.entitys.physicalField.dataUDC.dataUDCRule] 数据规范
+  * @apiSuccess {String} [data.entitys.physicalField.dataUDC.dataUDCValue] 数据值
+  * @apiSuccess {String} [data.entitys.physicalField.dataUDC.entityHash] 数据hash码
+  * @apiSuccess {String} [data.entitys.physicalField.dataUDC.entityId] 数据id
+  * @apiSuccess {String} [data.entitys.physicalField.dataUDC.entityType] 数据类型
   * */
   //查询
   @ResponseBody
@@ -78,49 +80,51 @@ public class TermInfoController {
     return service.selectTermAll(termInfoPojo);
   }
 
-  /*
+  /**
   * @api {get} /term/:Entityid 按id术语查询接口
+  * @apiVersion 1.0.0
+  * @apiSampleRequest http://192.168.2.61:8082/quantchiAPI/term/
   * @apiName  selectTerm
   * @apiGroup TermInfoController
-  * @apiParam {Integer} Entityid 术语id
+  * @apiParam {Integer} [Entityid] 术语id
   * @apiSuccess {String} code 成功或者错误代码200成功，500错误
   * @apiSuccess {String} msg  成功或者错误信息
-  * @apiSuccess {List} data 返回数据
-  * @apiSuccess {String} data.entityType 实体类型
-  * @apiSuccess {String} data.entityId 实体id
-  * @apiSuccess {String} data.entityHash 实体hash码
-  * @apiSuccess {String} data.entityName 实体名
-  * @apiSuccess {String} data.entityDesc 实体描述
-  * @apiSuccess {String} data.entityAlias 实体别名
-  * @apiSuccess {String} data.businessRule 规范描述
-  * @apiSuccess {String} data.techniqueRule sql语句
-  * @apiSuccess {String} data.entityStatus 实体状态
-  * @apiSuccess {String} data.controlDept 管理部门
-  * @apiSuccess {String} data.assistDept 分配部门
-  * @apiSuccess {String} data.devPolicy 个人信息分类
-  * @apiSuccess {String} data.regulatory
-  * @apiSuccess {String} data.logicType
-  * @apiSuccess {String} data.displayType 数据显示类型
-  * @apiSuccess {List} data.physicalField 物理字段
-  * @apiSuccess {String} data.physicalField.entityId 实体id
-  * @apiSuccess {String} data.physicalField.physicalFieldHash 物理字段hash码
-  * @apiSuccess {String} data.physicalField.physicalFieldId 物理字段id
-  * @apiSuccess {String} data.physicalField.physicalFieldDesc 物理字段描述
-  * @apiSuccess {String} data.physicalField.physicalTable 物理表
-  * @apiSuccess {String} data.physicalField.physicalDB 物理数据库
-  * @apiSuccess {String} data.physicalField.dataType 数据类型
-  * @apiSuccess {String} data.physicalField.dataLength 数据长度
-  * @apiSuccess {String} data.physicalField.dataPattern
-  * @apiSuccess {String} data.physicalField.dataUnit
-  * @apiSuccess {String} data.physicalField.fieldPartition
-  * @apiSuccess {List} data.physicalField.dataUDC 条件数据
-  * @apiSuccess {String} data.physicalField.dataUDC.dataUDCCode
-  * @apiSuccess {String} data.physicalField.dataUDC.dataUDCDesc 数据描述
-  * @apiSuccess {String} data.physicalField.dataUDC.dataUDCRule 数据规范
-  * @apiSuccess {String} data.physicalField.dataUDC.dataUDCValue 数据值
-  * @apiSuccess {String} data.physicalField.dataUDC.entityHash 数据hash码
-  * @apiSuccess {String} data.physicalField.dataUDC.entityId 数据id
-  * @apiSuccess {String} data.physicalField.dataUDC.entityType 数据类型
+  * @apiSuccess {List} [data] 返回数据
+  * @apiSuccess {String} [data.entityType] 实体类型
+  * @apiSuccess {String} [data.entityId] 实体id
+  * @apiSuccess {String} [data.entityHash] 实体hash码
+  * @apiSuccess {String} [data.entityName] 实体名
+  * @apiSuccess {String} [data.entityDesc] 实体描述
+  * @apiSuccess {String} [data.entityAlias] 实体别名
+  * @apiSuccess {String} [data.businessRule] 规范描述
+  * @apiSuccess {String} [data.techniqueRule] sql语句
+  * @apiSuccess {String} [data.entityStatus] 实体状态
+  * @apiSuccess {String} [data.controlDept] 管理部门
+  * @apiSuccess {String} [data.assistDept] 分配部门
+  * @apiSuccess {String} [data.devPolicy] 个人信息分类
+  * @apiSuccess {String} [data.regulatory]
+  * @apiSuccess {String} [data.logicType]
+  * @apiSuccess {String} [data.displayType] 数据显示类型
+  * @apiSuccess {List} [data.physicalField] 物理字段
+  * @apiSuccess {String} [data.physicalField.entityId] 实体id
+  * @apiSuccess {String} [data.physicalField.physicalFieldHash] 物理字段hash码
+  * @apiSuccess {String} [data.physicalField.physicalFieldId] 物理字段id
+  * @apiSuccess {String} [data.physicalField.physicalFieldDesc] 物理字段描述
+  * @apiSuccess {String} [data.physicalField.physicalTable] 物理表
+  * @apiSuccess {String} [data.physicalField.physicalDB] 物理数据库
+  * @apiSuccess {String} [data.physicalField.dataType] 数据类型
+  * @apiSuccess {String} [data.physicalField.dataLength] 数据长度
+  * @apiSuccess {String} [data.physicalField.dataPattern]
+  * @apiSuccess {String} [data.physicalField.dataUnit]
+  * @apiSuccess {String} [data.physicalField.fieldPartition]
+  * @apiSuccess {List} [data.physicalField.dataUDC] 条件数据
+  * @apiSuccess {String} [data.physicalField.dataUDC.dataUDCCode]
+  * @apiSuccess {String} [data.physicalField.dataUDC.dataUDCDesc] 数据描述
+  * @apiSuccess {String} [data.physicalField.dataUDC.dataUDCRule] 数据规范
+  * @apiSuccess {String} [data.physicalField.dataUDC.dataUDCValue] 数据值
+  * @apiSuccess {String} [data.physicalField.dataUDC.entityHash] 数据hash码
+  * @apiSuccess {String} [data.physicalField.dataUDC.entityId] 数据id
+  * @apiSuccess {String} [data.physicalField.dataUDC.entityType] 数据类型
   *
   *
   * */
@@ -135,6 +139,8 @@ public class TermInfoController {
 
   /**
    * @api {post} /term 术语添加接口
+   * @apiVersion 1.0.0
+   * @apiSampleRequest http://192.168.2.61:8082/quantchiAPI/term
    * @apiName TermInfoController
    * @apiGroup TermInfoController
    * @apiParam {Object[]} termGenInfos
