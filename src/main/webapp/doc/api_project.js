6430973c14c949a6761d1388d2070659db9d3ceb
loki <hjqi@quant-chi.com>
2018-07-04 09:29:59 +0800

    1.添加doc

---
+++ src/main/webapp/doc/api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "",
+  "version": "0.0.0",
+  "description": "",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-07-03T11:04:24.273Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
