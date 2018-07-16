530a5239710148a02ecd405f023bd0df6142a511
jxg <495373469@qq.com>
2018-07-16 17:08:52 +0800

    术语添加

--- src/main/java/com/quantchi/termInfo/serviceImpl/TermInfoServiceImpl.java
+++ src/main/java/com/quantchi/termInfo/serviceImpl/TermInfoServiceImpl.java
@@ -9,24 +9,35 @@ import com.quantchi.metadatamgr.data.entity.DSTableInfoDB;
 import com.quantchi.metadatamgr.data.entity.DSTableInfoDBExample;
 import com.quantchi.metadatamgr.data.mapper.DSEntityInfoDBMapper;
 import com.quantchi.metadatamgr.data.mapper.DSTableInfoDBMapper;
-import com.quantchi.termInfo.mapper.*;
-import com.quantchi.termInfo.pojo.*;
+import com.quantchi.termInfo.mapper.PhysicalFieldInfoMapper;
+import com.quantchi.termInfo.mapper.PhysicalTableInfoMapper;
+import com.quantchi.termInfo.mapper.TermInfoMapper;
+import com.quantchi.termInfo.mapper.TermLogicCatagoryMapper;
+import com.quantchi.termInfo.mapper.TermLogicFieldDraftMapper;
+import com.quantchi.termInfo.mapper.TermLogicFieldMapper;
+import com.quantchi.termInfo.mapper.TermMainInfoMapper;
+import com.quantchi.termInfo.pojo.PhysicalFieldInfo;
+import com.quantchi.termInfo.pojo.PhysicalTableInfo;
+import com.quantchi.termInfo.pojo.TermGenInfo;
+import com.quantchi.termInfo.pojo.TermInfoPojo;
+import com.quantchi.termInfo.pojo.TermLogicCatagory;
+import com.quantchi.termInfo.pojo.TermLogicField;
+import com.quantchi.termInfo.pojo.TermLogicFieldDraft;
+import com.quantchi.termInfo.pojo.TermLogicFieldExample;
+import com.quantchi.termInfo.pojo.TermMainInfo;
 import com.quantchi.termInfo.service.TermInfoService;
-
-import java.util.*;
-
-import javafx.util.Pair;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import javax.jdo.annotations.Transactional;
 import org.apache.commons.collections.map.HashedMap;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.solr.client.solrj.io.Tuple;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import javax.jdo.annotations.Transactional;
 
 /**
  * Created by 49537 on 2018/6/20.
@@ -88,12 +99,12 @@ public class TermInfoServiceImpl implements TermInfoService {
 
   private static final String ENTITY_TYPE = "1";
 
-  private final Map<String,String> _TypeMap = new HashMap<String,String>(){
-      {
-          put("Area","度量");
-          put("Text", "文本");
-      }
-    };
+  private final Map<String, String> _TypeMap = new HashMap<String, String>() {
+    {
+      put("Area", "度量");
+      put("Text", "文本");
+    }
+  };
 
   @Override
   public String selectTermAll(TermInfoPojo termInfoPojo) {
@@ -111,12 +122,12 @@ public class TermInfoServiceImpl implements TermInfoService {
           physical.put(str, map.get(str));
         }
         List<Map<String, Object>> udc = new ArrayList<>();
-        for(Map<String,Object> map2:list1){
-          if(map.get("entityId").equals(map2.get("entityId"))){
+        for (Map<String, Object> map2 : list1) {
+          if (map.get("entityId").equals(map2.get("entityId"))) {
             udc.add(map2);
           }
         }
-        physical.put("dataUDC",udc);
+        physical.put("dataUDC", udc);
         map1.put("physicalField", physical);
         ResultList.add(map1);
       }
@@ -180,96 +191,104 @@ public class TermInfoServiceImpl implements TermInfoService {
 
   @Transactional
   @Override
-  public String insertTerm(ArrayList<TermGenInfo> termGenInfos){
+  public String insertTerm(ArrayList<TermGenInfo> termGenInfos) {
     try {
       List<TermMainInfo> _termlist = new ArrayList<TermMainInfo>();
-      for(TermGenInfo termGenInfo:termGenInfos) {
-        if (termGenInfo.getTableInfo() == null
+      for (TermGenInfo termGenInfo : termGenInfos) {
+        /*if (termGenInfo.getTableInfo() == null
                 || termGenInfo.getFieldInfoList() == null)
-          throw new Exception("miss table or field or term info");
+          throw new Exception("miss table or field or term info");*/
 
-        Map<String,String> _entityIdMap = new HashMap<>();
+        Map<String, String> _entityIdMap = new HashMap<>();
 
-        for(PhysicalFieldInfo fieldInfo:termGenInfo.getFieldInfoList()){
-            String _uuid = UUID.randomUUID().toString().replace("-","");
+        if (termGenInfo.getFieldInfoList()!=null) {
+          for (PhysicalFieldInfo fieldInfo : termGenInfo.getFieldInfoList()) {
+            String _uuid = UUID.randomUUID().toString().replace("-", "");
             fieldInfo.setEntityId(_uuid);
-            _entityIdMap.put(fieldInfo.getPhysicalDb()+"."+fieldInfo.getPhysicalTable()+"."+fieldInfo.getPhysicalField(),_uuid);
+            _entityIdMap.put(
+                fieldInfo.getPhysicalDb() + "." + fieldInfo.getPhysicalTable() + "." + fieldInfo
+                    .getPhysicalField(), _uuid);
+          }
+
+          //插入表信息
+          physicalTableInfoMapper.insert(termGenInfo.getTableInfo());
+
+          //插入字段信息
+          physicalFieldInfoMapper.insertFields(termGenInfo.getFieldInfoList());
         }
-
-        //插入表信息
-        physicalTableInfoMapper.insert(termGenInfo.getTableInfo());
-
-        //插入字段信息
-        physicalFieldInfoMapper.insertFields(termGenInfo.getFieldInfoList());
-
         //术语主信息插入
-        if(termGenInfo.getTermMainInfo() != null){
-            termMainInfoMapper.insert(termGenInfo.getTermMainInfo());
-        }else{
-            for (PhysicalFieldInfo field : termGenInfo.getFieldInfoList()) {
-                TermMainInfo _term = new TermMainInfo();
-                //from insert field entity_id
-                _term.setEntityId(_entityIdMap.get(field.getPhysicalDb()+"."+field.getPhysicalTable()+"."+field.getPhysicalField()));
-                _term.setEntityType(ENTITY_TYPE);
-                _term.setEntityName(field.getPhysicalField());
-                _term.setEntityDesc(field.getPhysicalFieldDesc());
-                _term.setTechniqueRule(getTechCriteria(field));
-                _term.setEntityStatus("正常");
-                _term.setCreateTime(new Date());
+        if (termGenInfo.getTermMainInfo() != null) {
+          termMainInfoMapper.insert(termGenInfo.getTermMainInfo());
+        } else {
+          for (PhysicalFieldInfo field : termGenInfo.getFieldInfoList()) {
+            TermMainInfo _term = new TermMainInfo();
+            //from insert field entity_id
+            _term.setEntityId(_entityIdMap.get(
+                field.getPhysicalDb() + "." + field.getPhysicalTable() + "." + field
+                    .getPhysicalField()));
+            _term.setEntityType(ENTITY_TYPE);
+            _term.setEntityName(field.getPhysicalField());
+            _term.setEntityDesc(field.getPhysicalFieldDesc());
+            _term.setTechniqueRule(getTechCriteria(field));
+            _term.setEntityStatus("正常");
+            _term.setCreateTime(new Date());
 
-                //getLogicTypeAndDisplayType(field.getDataType())
-                String _type = getDisplayType(field.getDataType());
-                String _logicType = _TypeMap.get(_type);
-                _term.setDisplayType(_type);
-                _term.setLogicType(_logicType);
-                _termlist.add(_term);
-            }
+            //getLogicTypeAndDisplayType(field.getDataType())
+            String _type = getDisplayType(field.getDataType());
+            String _logicType = _TypeMap.get(_type);
+            _term.setDisplayType(_type);
+            _term.setLogicType(_logicType);
+            _termlist.add(_term);
+          }
 
-            if(_termlist.size() > 0)
-                termMainInfoMapper.insertTerms(_termlist);
+          if (_termlist.size() > 0) {
+            termMainInfoMapper.insertTerms(_termlist);
+          }
         }
-
-
-
 
 
       }
       return JsonResult.successJson(_termlist.size());
-    }catch (Exception e){
+    } catch (Exception e) {
       e.printStackTrace();
       return JsonResult.errorJson(e.getMessage());
     }
 
   }
 
-  private String getDisplayType(String type){
-      if(type.equals("double") || type.equals("float") || type.equals("int"))
-          return "Area";
-      if(type.equals("string") || type.indexOf("varchar")> -1 )
-          return "Text";
+  private String getDisplayType(String type) {
+    if (type.equals("double") || type.equals("float") || type.equals("int")) {
+      return "Area";
+    }
+    if (type.equals("string") || type.indexOf("varchar") > -1) {
+      return "Text";
+    }
 
-      return "";
+    return "";
   }
 
-  private String getTechCriteria(PhysicalFieldInfo field){
-    return "select " + field.getPhysicalField() + " from " + field.getPhysicalDb() + "." + field.getPhysicalTable();
+  private String getTechCriteria(PhysicalFieldInfo field) {
+    return "select " + field.getPhysicalField() + " from " + field.getPhysicalDb() + "." + field
+        .getPhysicalTable();
   }
 
-  private void insertFieldIntoOldDB(List<TermGenInfo> termGenInfos,Map<String,String> termLogicCategoryIdMap) throws Exception{
+  private void insertFieldIntoOldDB(List<TermGenInfo> termGenInfos,
+      Map<String, String> termLogicCategoryIdMap) throws Exception {
     try {
       for (TermGenInfo termInfo : termGenInfos) {
         PhysicalTableInfo _table = termInfo.getTableInfo();
         String _tableName = _table.getPhysicalTable();
         List<PhysicalFieldInfo> fieldInfos = termInfo.getFieldInfoList();
 
-        if(!termLogicCategoryIdMap.containsKey(_tableName))
+        if (!termLogicCategoryIdMap.containsKey(_tableName)) {
           continue;
+        }
 
         for (PhysicalFieldInfo field : fieldInfos) {
 
           TermLogicField _logicField = new TermLogicField();
-          _logicField.setEnglishName(_tableName+"."+field.getPhysicalField());
-          _logicField.setChineseName(_tableName+"."+field.getPhysicalField());
+          _logicField.setEnglishName(_tableName + "." + field.getPhysicalField());
+          _logicField.setChineseName(_tableName + "." + field.getPhysicalField());
           //_logicField.setChineseName(field.getPhysicalFieldDesc()==null?"undefined":field.getPhysicalFieldDesc());
           //_logicField.setEnglishName(field.getPhysicalField());
           _logicField.setLogicCate(termLogicCategoryIdMap.get(_tableName));
@@ -290,83 +309,94 @@ public class TermInfoServiceImpl implements TermInfoService {
             _logicField.setLogicOnlineId(_logicField_draft.getId());*/
           try {
             termLogicFieldMapper.insert(_logicField);
-          }catch (Exception e){
-            if(e.getMessage().indexOf("Duplicate entry") == -1)
+          } catch (Exception e) {
+            if (e.getMessage().indexOf("Duplicate entry") == -1) {
               throw e;
+            }
           }
 
-            TermLogicFieldDraft _logicField_draft = new TermLogicFieldDraft();
-            _logicField_draft.setEnglishName(_tableName+"."+field.getPhysicalField());
-            _logicField_draft.setChineseName(_tableName+"."+field.getPhysicalField());
-            //_logicField_draft.setChineseName(field.getPhysicalFieldDesc()==null?"undefined":field.getPhysicalFieldDesc());
-            //等插入表后返回
-            _logicField_draft.setLogicCate(termLogicCategoryIdMap.get(_tableName));
-            _logicField_draft.setTechCriteria(getTechCriteria(field));
-            _logicField_draft.setStatus(2);
-            //_logicField_draft.setLogicOnlineId(0);
-            _logicField_draft.setIsSum("false");
-            _logicField_draft.setIsGroup("false");
-            _logicField_draft.setIsUniq("false");
+          TermLogicFieldDraft _logicField_draft = new TermLogicFieldDraft();
+          _logicField_draft.setEnglishName(_tableName + "." + field.getPhysicalField());
+          _logicField_draft.setChineseName(_tableName + "." + field.getPhysicalField());
+          //_logicField_draft.setChineseName(field.getPhysicalFieldDesc()==null?"undefined":field.getPhysicalFieldDesc());
+          //等插入表后返回
+          _logicField_draft.setLogicCate(termLogicCategoryIdMap.get(_tableName));
+          _logicField_draft.setTechCriteria(getTechCriteria(field));
+          _logicField_draft.setStatus(2);
+          //_logicField_draft.setLogicOnlineId(0);
+          _logicField_draft.setIsSum("false");
+          _logicField_draft.setIsGroup("false");
+          _logicField_draft.setIsUniq("false");
 
-
-            if(_logicField.getId() == null){
-                TermLogicFieldExample _ex = new TermLogicFieldExample();
-                _ex.createCriteria().andEnglishNameEqualTo(_tableName+"."+field.getPhysicalField());
-                List<TermLogicField> _res = termLogicFieldMapper.selectByExample(_ex);
-                TermLogicField _logfield =  _res.get(0);
-                _logicField_draft.setLogicOnlineId(_logfield.getId());
-            }else
-                _logicField_draft.setLogicOnlineId(_logicField.getId());
-            try {
-                termLogicFieldDraftMapper.insert(_logicField_draft);
-            }catch (Exception e){
-                if(e.getMessage().indexOf("Duplicate entry") == -1)
-                    throw e;
+          if (_logicField.getId() == null) {
+            TermLogicFieldExample _ex = new TermLogicFieldExample();
+            _ex.createCriteria().andEnglishNameEqualTo(_tableName + "." + field.getPhysicalField());
+            List<TermLogicField> _res = termLogicFieldMapper.selectByExample(_ex);
+            TermLogicField _logfield = _res.get(0);
+            _logicField_draft.setLogicOnlineId(_logfield.getId());
+          } else {
+            _logicField_draft.setLogicOnlineId(_logicField.getId());
+          }
+          try {
+            termLogicFieldDraftMapper.insert(_logicField_draft);
+          } catch (Exception e) {
+            if (e.getMessage().indexOf("Duplicate entry") == -1) {
+              throw e;
             }
+          }
 
-            try {
-                termLogicFieldDraftMapper.insert(_logicField_draft);
-            }catch (Exception e){
-                if(e.getMessage().indexOf("Duplicate entry") == -1)
-                    throw e;
+          try {
+            termLogicFieldDraftMapper.insert(_logicField_draft);
+          } catch (Exception e) {
+            if (e.getMessage().indexOf("Duplicate entry") == -1) {
+              throw e;
             }
+          }
 
         }
 
       }
-    } catch(Exception e){
-         throw new Exception(e.getMessage());
+    } catch (Exception e) {
+      throw new Exception(e.getMessage());
     }
 
   }
+
   @Transactional
   @Override
-  public String insertTermLogic(Map<String,Object> requestMap) {
-    try{
-      List<TermGenInfo> termGenInfos = (List)requestMap.get("termGenInfoList");
+  public String insertTermLogic(Map<String, Object> requestMap) {
+    try {
+      List<TermGenInfo> termGenInfos = (List) requestMap.get("termGenInfoList");
       String _termGenInfoStr = JSONObject.toJSONString(termGenInfos);
-      termGenInfos  = JSONObject.parseArray(_termGenInfoStr,TermGenInfo.class);
-      List<TermLogicCatagory> termLogicCatagories = (List<TermLogicCatagory>)requestMap.get("termLogicCatagoryEntityList");
+      termGenInfos = JSONObject.parseArray(_termGenInfoStr, TermGenInfo.class);
+      List<TermLogicCatagory> termLogicCatagories = (List<TermLogicCatagory>) requestMap
+          .get("termLogicCatagoryEntityList");
       String _logicCataStr = JSONObject.toJSONString(termLogicCatagories);
-      termLogicCatagories  = JSONObject.parseArray(_logicCataStr,TermLogicCatagory.class);
-      Map<String,String> termLogicCategoryIdMap = new HashMap<>();
-      for(TermLogicCatagory termLogicCatagory : termLogicCatagories){
+      termLogicCatagories = JSONObject.parseArray(_logicCataStr, TermLogicCatagory.class);
+      Map<String, String> termLogicCategoryIdMap = new HashMap<>();
+      for (TermLogicCatagory termLogicCatagory : termLogicCatagories) {
         termLogicCatagoryMapper.insert(termLogicCatagory);
 
         DSEntityInfoDBExample dsEntityInfoDBExample = new DSEntityInfoDBExample();
-        dsEntityInfoDBExample.createCriteria().andIdEqualTo(Integer.parseInt(termLogicCatagory.getDataSourceId()));
-        List<DSEntityInfoDB> dsEntityInfoDBList = dsEntityInfoDBMapper.selectByExample(dsEntityInfoDBExample);
+        dsEntityInfoDBExample.createCriteria()
+            .andIdEqualTo(Integer.parseInt(termLogicCatagory.getDataSourceId()));
+        List<DSEntityInfoDB> dsEntityInfoDBList = dsEntityInfoDBMapper
+            .selectByExample(dsEntityInfoDBExample);
 
         DSTableInfoDBExample dsTableInfoDBExample = new DSTableInfoDBExample();
-        dsTableInfoDBExample.createCriteria().andDatasourceIdEqualTo(termLogicCatagory.getDataSourceName());
-        List<DSTableInfoDB> dsTableInfoDBList = dsTableInfoDBMapper.selectByExample(dsTableInfoDBExample);
+        dsTableInfoDBExample.createCriteria()
+            .andDatasourceIdEqualTo(termLogicCatagory.getDataSourceName());
+        List<DSTableInfoDB> dsTableInfoDBList = dsTableInfoDBMapper
+            .selectByExample(dsTableInfoDBExample);
 
-        for(DSTableInfoDB dsTableInfoDB : dsTableInfoDBList){
-          if(dsEntityInfoDBList.get(0).getMainTable().contains(dsTableInfoDB.getTableEnglishName()) || dsEntityInfoDBList.get(0).getNonMainTable().contains(dsTableInfoDB.getTableEnglishName())){
+        for (DSTableInfoDB dsTableInfoDB : dsTableInfoDBList) {
+          if (dsEntityInfoDBList.get(0).getMainTable().contains(dsTableInfoDB.getTableEnglishName())
+              || dsEntityInfoDBList.get(0).getNonMainTable()
+              .contains(dsTableInfoDB.getTableEnglishName())) {
             TermLogicCatagory termLogicCatagory2 = new TermLogicCatagory();
-            if(dsTableInfoDB.getTableChineseName() != null){
+            if (dsTableInfoDB.getTableChineseName() != null) {
               termLogicCatagory2.setCategoryName(dsTableInfoDB.getTableChineseName());
-            }else{
+            } else {
               termLogicCatagory2.setCategoryName(dsTableInfoDB.getTableEnglishName());
             }
             String tableName = dsTableInfoDB.getTableEnglishName().split("\\.")[1];
@@ -375,16 +405,18 @@ public class TermInfoServiceImpl implements TermInfoService {
             termLogicCatagory2.setCreateTime(new Date());
             termLogicCatagory2.setParentId(termLogicCatagory.getId());
             termLogicCatagoryMapper.insert(termLogicCatagory2);
-            termLogicCategoryIdMap.put(tableName,termLogicCatagory.getParentId()+"_"+termLogicCatagory.getId()+"_"+termLogicCatagory2.getId());
+            termLogicCategoryIdMap.put(tableName,
+                termLogicCatagory.getParentId() + "_" + termLogicCatagory.getId() + "_"
+                    + termLogicCatagory2.getId());
             //termLogicCategoryIdMap.put(tableName,termLogicCatagory.getParentId()+"_"+dsTableInfoDB.getId()+"_"+termLogicCatagory.getId());
           }
 
         }
 
       }
-      insertFieldIntoOldDB(termGenInfos,termLogicCategoryIdMap);
+      insertFieldIntoOldDB(termGenInfos, termLogicCategoryIdMap);
       return JsonResult.successJson();
-    }catch (Exception e){
+    } catch (Exception e) {
       e.printStackTrace();
       return JsonResult.errorJson(e.getMessage());
     }
