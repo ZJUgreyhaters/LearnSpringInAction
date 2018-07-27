fc0adf1e56c9e6c9a529fe2c8b2e12c9fcb941d2
jxg <495373469@qq.com>
2018-07-27 09:41:42 +0800

    文件上传

--- src/main/java/com/quantchi/termInfo/controller/TermFileController.java
+++ src/main/java/com/quantchi/termInfo/controller/TermFileController.java
@@ -4,22 +4,17 @@ import com.quantchi.common.ExcelUtil;
 import com.quantchi.common.JsonResult;
 import com.quantchi.common.ResultCode;
 import com.quantchi.common.SQLQueryConfig;
-import java.util.ArrayList;
+import com.quantchi.termInfo.service.TermFileService;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import net.sf.json.JSONArray;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
 /**
@@ -31,6 +26,9 @@ public class TermFileController {
 
   @Autowired
   private SQLQueryConfig sqlQueryConfig;
+
+  @Autowired
+  private TermFileService termFileService;
 
   /**
    * @api {post} /term/import 文件上传接口
@@ -46,7 +44,8 @@ public class TermFileController {
    */
 
   @ResponseBody
-  @RequestMapping(value = "/import", method = {RequestMethod.POST}, produces = "application/json;charset=UTF-8")
+  @RequestMapping(value = "/import", method = {
+      RequestMethod.POST}, produces = "application/json;charset=UTF-8")
   public String importExcel(
       @RequestParam(value = "file", required = false) MultipartFile file, String typeOne,
       String typeTwo
@@ -59,56 +58,82 @@ public class TermFileController {
       map.put(split1[0], split1[1]);
     }
     List<String[]> result;
+
     try {
       result = ExcelUtil.readExcel(file);
     } catch (Exception e) {
       return JsonResult
           .errorJson(ResultCode.FILE_FORMAT_ERROR, "excel格式错误，上传的模板文件请另存为新的xlsx文件后上传");
     }
-    try{
-    String[] strings = result.get(0);
-    List<String> list = new LinkedList<>();
-    for (String str : strings) {
-      if (map.get(str)!=null) {
-        list.add(map.get(str));
-      }
-    }
-    if(list.size()!=strings.length){
-      return JsonResult.errorJson("配置字段与传入字段不匹配");
-    }
-    List<Map<String, Map<String, Object>>> list1 = new ArrayList<>();
-    Map<String, Map<String, Object>> map3= new HashMap<>();
-    if ("standard".equals(typeOne)&&"mapping".equals(typeTwo)) {
-      return JsonResult.successJson("上传成功！");
-    } else if ("target".equals(typeOne) && "mapping".equals(typeTwo)) {
-      return JsonResult.successJson("上传成功！");
-    } else {
-      for (int i = 1; i < result.size(); i++) {
-        String[] values = result.get(i);
-        Map<String, Object> map1 = new HashMap<>();
-        for (int j = 0; j < values.length; j++) {
-          map1.put(list.get(j), values[j]);
+
+    try {
+      String[] strings = result.get(0);
+      List<String> list = new LinkedList<>();
+      for (String str : strings) {
+        if (map.get(str) != null) {
+          list.add(map.get(str));
         }
-        map3.put("termMainInfo",map1);
-        list1.add(map3);
       }
-      JSONArray jsonArray2 = JSONArray.fromObject(list1);
 
-      RestTemplate restTemplate = new RestTemplate();
-      HttpHeaders headers = new HttpHeaders();
-      MediaType type = MediaType.parseMediaType("application/json; charset=UTF-8");
-      headers.setContentType(type);
-      headers.add("Accept", MediaType.APPLICATION_JSON.toString());
-      HttpEntity<String> formEntity = new HttpEntity<String>(jsonArray2.toString(), headers);
+      if (list.size() != strings.length) {
+        return JsonResult.errorJson("配置字段与传入字段不匹配");
+      }
 
-      String re = restTemplate.postForObject("http://192.168.2.136:8082/term", formEntity, String.class);
-      if(re.contains("code:200")){
+      if ("standard".equals(typeOne) && "mapping".equals(typeTwo)) {
+        for (int i = 1; i < result.size(); i++) {
+          String[] values = result.get(i);
+          Map<String, Object> map1 = new HashMap<>();
+          for (int j = 0; j < values.length; j++) {
+            map1.put(list.get(j), values[j]);
+          }
+          List<Map<String, Object>> resultList = termFileService.selectStandard(map1);
+          if (resultList.isEmpty() || resultList == null) {
+            termFileService.insertStandard(map1);
+          } else {
+            termFileService.updateStandard(map1);
+          }
+        }
         return JsonResult.successJson("上传成功！");
-      }else{
-        return JsonResult.errorJson("上传失败！");
+      } else if ("target".equals(typeOne) && "mapping".equals(typeTwo)) {
+        for (int i = 1; i < result.size(); i++) {
+          String[] values = result.get(i);
+          Map<String, Object> map1 = new HashMap<>();
+          for (int j = 0; j < values.length; j++) {
+            map1.put(list.get(j), values[j]);
+          }
+          List<Map<String, Object>> PhysicalInfo = termFileService.selectPhysicalInfo(map1);
+          map1.put("fieldId", PhysicalInfo.get(0).get("entity_id"));
+          List<Map<String, Object>> PhysicalFile = termFileService.selectPhysicalFile(map1);
+          if (PhysicalFile.isEmpty() || PhysicalFile == null) {
+            termFileService.insertPhysicalFile(map1);
+          } else {
+            termFileService.updatePhysicalFile(map1);
+          }
+        }
+        return JsonResult.successJson("上传成功！");
+      } else if ("standard".equals(typeOne) && "common".equals(typeTwo)) {
+        for (int i = 1; i < result.size(); i++) {
+          String[] values = result.get(i);
+          Map<String, Object> map1 = new HashMap<>();
+          for (int j = 0; j < values.length; j++) {
+            map1.put(list.get(j), values[j]);
+          }
+          String domainId = termFileService.selectDomainByName(map1);
+          map1.put("domainId", domainId);
+          String entityCategory = termFileService.selectStandardCategory(map1);
+          map1.put("entityCategory", entityCategory);
+          List<Map<String, Object>> standardMain = termFileService.selectStandardMain(map1);
+          if (standardMain.isEmpty() || standardMain == null) {
+            termFileService.insertStandardMain(map1);
+          } else {
+            termFileService.updateStandardMain(map1);
+          }
+        }
+        return JsonResult.successJson("上传成功！");
       }
-    }
-    }catch (Exception e) {
+      return JsonResult.successJson("上传失败！");
+    } catch (Exception e) {
+      e.printStackTrace();
       return JsonResult.errorJson("上传失败！");
     }
   }
--- src/main/java/com/quantchi/termInfo/service/TermFileService.java
+++ src/main/java/com/quantchi/termInfo/service/TermFileService.java
@@ -1,8 +1,35 @@
 package com.quantchi.termInfo.service;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by 49537 on 2018/7/26.
  */
 public interface TermFileService {
+
+  void insertStandard(Map<String, Object> map);
+
+  List<Map<String, Object>> selectStandard(Map<String, Object> map);
+
+  void updateStandard(Map<String, Object> map);
+
+  List<Map<String, Object>> selectPhysicalInfo(Map<String, Object> map);
+
+  void insertPhysicalFile(Map<String, Object> map);
+
+  void updatePhysicalFile(Map<String, Object> map);
+
+  List<Map<String, Object>> selectPhysicalFile(Map<String, Object> map);
+
+  String selectDomainByName(Map<String, Object> map);
+
+  String selectStandardCategory(Map<String, Object> map);
+
+  List<Map<String, Object>> selectStandardMain(Map<String, Object> map);
+
+  void insertStandardMain(Map<String, Object> map);
+
+  void updateStandardMain(Map<String, Object> map);
 
 }
