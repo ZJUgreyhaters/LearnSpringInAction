97f7a6e17ef7b696d24780aff56fccaa064efbd8
ddd <lajiyou9@163.com>
2018-08-29 13:06:17 +0800

    注释

--- src/main/java/com/quantchi/authority/controller/AuthorityDetailController.java
+++ src/main/java/com/quantchi/authority/controller/AuthorityDetailController.java
@@ -22,7 +22,33 @@ public class AuthorityDetailController {
     private AuthorityDetailService authoritydetailService;
 
 
-    /*插入数据权限
+    /** @api {post} /api/addDataAuth 添加新的数据权限
+     * @apiVersion 1.0.0
+     * @apiSampleRequest http://192.168.2.61:8082/quantchiAPI/api/addDataAuth
+     * @apiName addDataAuth
+     * @apiGroup AuthorityDetailController
+     * @apiParam {Object} [authority] 权限
+     * @apiParam {String} [authority.c_authname] 权限名称
+     * @apiParam {String} [authority.c_isenable] 是否生效
+     * @apiParam {String} [authority.c_authtype] 权限类型  0:功能权限  1:数据权限'
+     * @apiParam {Int} [authority.l_authid] 权限id   因为还未插入表 填0
+     * @apiParam {List} [dataTabDetail] 表数据权限明细 可以装入多个
+     * @apiParam {Int} [dataTabDetail.l_datatabledetailid] 表权限ID    因为还未插入表 填0
+     * @apiParam {String} [dataTabDetail.c_tablename] 表名字
+     * @apiParam {String} [dataTabDetail.c_isenable]  是否有效
+     * @apiParam {List} [dataColDetail] 表字段数据权限明细 可以装入多个
+     * @apiParam {Int} [dataColDetail.l_columndetailid] 表字段权限ID    因为还未插入表 填0
+     * @apiParam {String} [dataColDetail.c_tablename] 表名字
+     * @apiParam {String} [dataColDetail.c_column] 表字段名字
+     * @apiParam {String} [dataColDetail.c_isenable]  是否有效
+     * @apiParam {List} [dataLineDetail] 行数据权限明细 可以装入多个
+     * @apiParam {Int} [dataLineDetail.l_linedetailid] 行数据权限ID    因为还未插入表 填0
+     * @apiParam {String} [dataLineDetail.c_tablename] 表名字
+     * @apiParam {String} [dataLineDetail.c_column] 表字段名字
+     * @apiParam {String} [dataLineDetail.c_fiterdesc] 过滤描述
+     * @apiParam {String} [dataLineDetail.c_fiter]  过滤条件
+     * @apiParam {String} [dataLineDetail.c_isenable]  是否有效
+     *
     *例子
     *{"authority":{"c_authname":"quantchix","c_isenable":"1","c_authtype":"0","l_authid":0},
     * "dataTabDetail":[{"l_datatabledetailid":1,"c_tablename":"tcustomerinfo","c_isenable":0},{"l_datatabledetailid":1,"c_tablename":"taccoinfo","c_isenable":0}],
