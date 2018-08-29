aeda5e8c65bcee79e55325d6db4fa660e9f5701d
your name <your_email@youremail.com>
2018-08-29 11:18:49 +0800

    sqlparser

---
+++ src/main/java/com/quantchi/authority/sqlparser/RowPermission.java
@@ -0,0 +1,53 @@
+package com.quantchi.authority.sqlparser;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @ClassName: RowPermission
+ * @Description: TODO
+ * @Author: wbchen
+ * @Date: 2018/8/29 10:05
+ * @Version 1.0.0
+ **/
+public class RowPermission {
+    /*
+    存储行权限
+     */
+    private Map<String, List<String>> rowPermission = new HashMap<>();
+
+    RowPermission(){
+
+    }
+
+    public void addSimpleRowRule(String tableName,String rowRule){
+        if(rowPermission.containsKey(tableName)){
+            List<String> rowSimpleRule = rowPermission.get(tableName);
+            rowSimpleRule.add(rowRule);
+            rowPermission.replace(tableName, rowSimpleRule);
+        }
+        else{
+            List<String> tmp = new ArrayList<>();
+            tmp.add(rowRule);
+            rowPermission.put(tableName, tmp);
+        }
+    }
+
+    public JSONObject getRowPermissionJson(){
+        JSONObject reRowJson = new JSONObject();
+        for(String key: rowPermission.keySet()){
+            List<String> rowRule = rowPermission.get(key);
+            JSONArray rowJson = new JSONArray();
+            for(int i = 0;i < rowRule.size();i++){
+                rowJson.add(rowRule.get(i));
+            }
+            reRowJson.put(key,rowJson);
+        }
+        return reRowJson;
+    }
+}
