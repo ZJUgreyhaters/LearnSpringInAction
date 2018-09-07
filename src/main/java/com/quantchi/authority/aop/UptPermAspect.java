4909aab4ac255c053bf4ed85b2a053ccafe31075
loki <hjqi@quant-chi.com>
2018-09-07 22:01:55 +0800

    1.add call permission update

--- src/main/java/com/quantchi/authority/aop/UptPermAspect.java
+++ src/main/java/com/quantchi/authority/aop/UptPermAspect.java
@@ -1,10 +1,12 @@
 package com.quantchi.authority.aop;
 
+import com.quantchi.authority.shiro.ShiroService;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -13,16 +15,20 @@ public class UptPermAspect {
 
 	private static final Logger logger = LoggerFactory.getLogger(UptPermAspect.class);
 
+	@Autowired
+	ShiroService shiroService;
+
 	public UptPermAspect(){
 		System.out.print("******UptPermAspect is loading*****");
 	}
 
-	@Pointcut("execution(public * com.quantchi.intelquery.*.*.execsql*(..))")
+	@Pointcut("execution(public * com.quantchi.authority.serviceImpl.*.addAuthAndDataDetail(..))")
 	public void callUptMethod(){}
 
 	@After("callUptMethod()")
 	public void after() {
-		System.out.println("method after");
+		System.out.println(" addAuthAndDataDetail method after");
+		shiroService.updatePermission();
 	}
 
 }
